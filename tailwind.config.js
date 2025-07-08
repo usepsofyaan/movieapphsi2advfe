b604d4499c72951432a00ff9f4b37fc8d2797dfd
Nama Kamu <emailkamu@example.com>
2025-07-08 11:07:29 +0700

    add font lato

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ["var(--font-lato)"],
+      },
+    },
+  },
+  plugins: [],
+};
