ea9fc9c45917c76d690c59fdd1633bc1ae5cbbb4
Nama Kamu <emailkamu@example.com>
2025-07-08 09:40:07 +0700

    add eyeicon

--- app/login/page.js
+++ app/login/page.js
@@ -1,15 +1,13 @@
 "use client";
 import { useState } from "react";
 import Image from "next/image";
-import { EyeIcon, EyeOffIcon } from "@heroicons/react/solid";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div
-      className="min-h-screen flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/backlogin.jpg')" }} // ganti dengan nama file gambar kamu
-    >
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/backlogin.jpg')" }}>
       <div className="bg-[#181A1CD6] rounded-xl shadow-xl p-6 w-full max-w-md">
         {/* Logo */}
         <div className="flex justify-center mb-4">
@@ -17,7 +15,6 @@ export default function Login() {
         </div>
 
         {/* Teks Selamat Datang */}
-
         <div className="text-center mb-6">
           <h2 className="text-2xl font-bold text-[#ffffff] mb-[5px]">Masuk</h2>
           <h5 className="text-2xl font-bold text-[#ffffff]">Selamat Datang</h5>
@@ -27,12 +24,18 @@ export default function Login() {
         <form>
           <div className="mb-4">
             <label className="block mb-1 font-medium text-[#ffffff]">Username</label>
-            <input type={showPassword ? "text" : "password"} className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff]" placeholder="Masukkan username" />
+            <input type="text" className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff]" placeholder="Masukkan username" />
           </div>
 
+          {/* Kata sandi*/}
           <div className="mb-4">
             <label className="block mb-1 font-medium text-[#ffffff]">Kata Sandi</label>
-            <input type="password" className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff]" placeholder="Masukkan kata sandi" />
+            <div className="relative">
+              <input type={showPassword ? "text" : "password"} className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff] pr-10 bg-transparent" placeholder="Masukkan kata sandi" />
+              <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white">
+                {showPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              </button>
+            </div>
           </div>
 
           {/* Link Daftar & Lupa Sandi */}
