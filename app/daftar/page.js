e3e303a4f7584de882c6f4978afc90f842d44f10
Nama Kamu <emailkamu@example.com>
2025-07-08 12:17:02 +0700

    add page signup

---
+++ app/daftar/page.js
@@ -0,0 +1,76 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+
+export default function Daftar() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword2, setShowPassword2] = useState(false);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: "url('/backlogin.jpg')" }}>
+      <div className="bg-[#181A1CD6] rounded-xl shadow-xl p-6 w-full max-w-md">
+        {/* Logo */}
+        <div className="flex justify-center mb-4">
+          <Image src="/logo.jpg" alt="Logo" width={80} height={80} />
+        </div>
+
+        {/* Teks Selamat Datang */}
+        <div className="text-center mb-6">
+          <h2 className="text-2xl font-bold text-[#ffffff] mb-[5px]">Daftar</h2>
+          <h5 className="text-2xl font-bold text-[#ffffff]">Selamat Datang</h5>
+        </div>
+
+        {/* Form Login */}
+        <form>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-[#ffffff]">Username</label>
+            <input type="text" className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff]" placeholder="Masukkan username" />
+          </div>
+
+          {/* Kata sandi*/}
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-[#ffffff]">Kata Sandi</label>
+            <div className="relative">
+              <input type={showPassword ? "text" : "password"} className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff] pr-10 bg-transparent" placeholder="Masukkan kata sandi" />
+              <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white">
+                {showPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              </button>
+            </div>
+          </div>
+          <div className="mb-4">
+            <label className="block mb-1 font-medium text-[#ffffff]">Konfirmasi Kata Sandi</label>
+            <div className="relative">
+              <input type={showPassword2 ? "text" : "password"} className="w-full border rounded-[24px] px-3 py-2 text-[#ffffff] pr-10 bg-transparent" placeholder="Masukkan kata sandi" />
+              <button type="button" onClick={() => setShowPassword2(!showPassword2)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white">
+                {showPassword2 ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              </button>
+            </div>
+          </div>
+
+          {/* Link Daftar & Lupa Sandi */}
+          <div className="flex justify-between text-sm mb-4">
+            <a href="/login" className="text-[#ffffff]">
+              Sudah punya akun? Masuk
+            </a>
+            <a href="/lupa-password" className="text-[#ffffff]">
+              Lupa kata sandi?
+            </a>
+          </div>
+
+          {/* Tombol Masuk */}
+          <button type="submit" className="w-full bg-[#3D4142] text-white py-2 rounded-[24px] font-semibold">
+            Daftar
+          </button>
+
+          {/* Atau Masuk dengan Google */}
+          <div className="text-center mt-4 text-sm text-gray-600">atau</div>
+          <button type="button" className="w-full border border-gray-300 mt-2 py-2 rounded-[24px] flex items-center justify-center hover:bg-gray-100">
+            <Image src="/google.png" alt="Google" width={20} height={20} className="mr-2" />
+            Daftar dengan Google
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,33 +4,15 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">app/page.js</code>.
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link
-            href="/login"
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-          >
-            <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
-            Login
-          </Link>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <Link
+          href="/login"
+          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        >
+          <Image className="dark:invert" src="/vercel.svg" alt="Vercel logomark" width={20} height={20} />
+          Login
+        </Link>
+      </div>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
