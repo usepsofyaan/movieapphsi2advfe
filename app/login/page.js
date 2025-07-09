289a67375cb7db1ea827e5097a2fe16c8577ba55
Nama Kamu <emailkamu@example.com>
2025-07-09 15:42:35 +0700

    add border

--- app/login/page.js
+++ app/login/page.js
@@ -49,13 +49,13 @@ export default function Login() {
           </div>
 
           {/* Tombol Masuk */}
-          <button type="submit" className="w-full bg-[#3D4142] text-white py-2 rounded-[24px] font-semibold">
+          <button type="submit" className="w-full bg-[#3D4142] border border-[#E7E3FC3B] text-white py-2 rounded-[24px] font-semibold">
             Masuk
           </button>
 
           {/* Atau Masuk dengan Google */}
           <div className="text-center mt-4 text-sm text-gray-600">atau</div>
-          <button type="button" className="w-full border border-gray-300 mt-2 py-2 rounded-[24px] flex items-center justify-center hover:bg-gray-100">
+          <button type="button" className="w-full border border-[#E7E3FC3B] mt-2 py-2 rounded-[24px] flex items-center justify-center hover:bg-gray-100">
             <Image src="/google.png" alt="Google" width={20} height={20} className="mr-2" />
             Masuk dengan Google
           </button>
