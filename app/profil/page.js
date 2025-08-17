9ea2788cf13ff8b5c7763069e6035cd3fa70ea65
Nama Kamu <emailkamu@example.com>
2025-08-17 11:56:45 +0700

    improve: style input in profil

--- app/profil/page.js
+++ app/profil/page.js
@@ -117,12 +117,12 @@ export default function Profil() {
     <>
       <Navbar />
       <div className="max-w-3xl mx-auto p-6">
-        <h1 className="text-2xl font-bold mb-6">Profil Saya</h1>
+        <h1 className="text-[20px] md-text-[32px] font-bold mb-6">Profil Saya</h1>
 
         <div className="flex items-center mb-6">
           <img src={preview || "/avatar.png"} alt="Foto Profil" className="w-[140px] h-[140px] rounded-full object-cover border" />
           <div className="ml-4">
-            <label className="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer">
+            <label className="px-4 py-2 text-[#3254FF] border border-[#3254FF] rounded-[48px] cursor-pointer">
               Ubah Foto
               <input type="file" accept="image/*" className="hidden" onChange={handleFileChange} />
             </label>
@@ -133,17 +133,17 @@ export default function Profil() {
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <label className="block mb-1 font-medium">Nama Pengguna</label>
-            <input type="text" name="nama" value={formData.nama} onChange={handleChange} className="w-full border rounded px-3 py-2" placeholder="Masukkan nama pengguna" />
+            <input type="text" name="nama" value={formData.nama} onChange={handleChange} className="w-full border border-[#E7E3FC3B] rounded-[8px] px-3 py-2" placeholder="Masukkan nama pengguna" />
           </div>
 
           <div>
             <label className="block mb-1 font-medium">Email</label>
-            <input type="email" name="email" value={formData.email} onChange={handleChange} className="w-full border rounded px-3 py-2" placeholder="Masukkan email" />
+            <input type="email" name="email" value={formData.email} onChange={handleChange} className="w-full border border-[#E7E3FC3B] rounded-[8px] px-3 py-2" placeholder="Masukkan email" />
           </div>
 
           <div>
             <label className="block mb-1 font-medium">Password</label>
-            <input type="password" name="password" value={formData.password} onChange={handleChange} className="w-full border rounded px-3 py-2" placeholder="Masukkan password baru" />
+            <input type="password" name="password" value={formData.password} onChange={handleChange} className="w-full border border-[#E7E3FC3B] rounded-[8px] px-3 py-2" placeholder="Masukkan password baru" />
           </div>
 
           <button type="submit" disabled={loading} className={`px-6 py-2 rounded-[24px] text-white ${loading ? "bg-[#09147A]" : "bg-[#09147A] hover:bg-[#09147A]-600"}`}>
